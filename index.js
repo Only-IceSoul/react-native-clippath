43e6174a3f80caca7aea86b4960dc3bd2539d12f
Juan <justinjlf21@gmail.com>
2021-09-24 00:21:02 -0400

    ver 1.0.7

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { Platform,requireNativeComponent } from 'react-native';
 import ClipPathWeb from './src/ClipPathWeb'
 
 const ClipPathMobile =  requireNativeComponent('JJSClipPath',null)
+const ClipPathMobileN = requireNativeComponent('JJSClipPathNone',null) 
 
 export const ClipPathView = Platform.OS == 'android' || Platform.OS == 'ios' ? ClipPathMobile : ClipPathWeb
-export const ClipPathViewN = Platform.OS == 'android' ? requireNativeComponent('JJSClipPathNone',null) : (Platform.OS == 'ios' ? ClipPathMobile : ClipPathWeb)
+export const ClipPathViewN = Platform.OS == 'android' ? ClipPathMobileN : (Platform.OS == 'ios' ? ClipPathMobile : ClipPathWeb)
